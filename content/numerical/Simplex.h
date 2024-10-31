36b0af345c0ea70bc0f0fbbd2755157d6bd0013d
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2024-10-31 18:58:53 +0600

    Update Simplex.h

--- content/numerical/Simplex.h
+++ content/numerical/Simplex.h
@@ -1,18 +1,35 @@
-/**
- * Author: Stanford
- * Source: Stanford Notebook
- * License: MIT
- * Description: Solves a general linear maximization problem: maximize $c^T x$ subject to $Ax \le b$, $x \ge 0$.
- * Returns -inf if there is no solution, inf if there are arbitrarily good solutions, or the maximum value of $c^T x$ otherwise.
- * The input vector is set to an optimal $x$ (or in the unbounded case, an arbitrary solution fulfilling the constraints).
- * Numerical stability is not guaranteed. For better performance, define variables such that $x = 0$ is viable.
- * Usage:
- * vvd A = {{1,-1}, {-1,1}, {-1,-2}};
- * vd b = {1,1,-4}, c = {-1,-1}, x;
- * T val = LPSolver(A, b, c).solve(x);
- * Time: O(NM * \#pivots), where a pivot may be e.g. an edge relaxation. O(2^n) in the general case.
- * Status: seems to work?
- */
+// Author: Stanford
+// Solves a general linear maximization problem.
+// The input is taken in the standard form for linear programs.
+//
+// Maximize c1.x1 + c2.x2 + ... + cn.xn
+// Subject to:
+// a11.x1 + a12.x2 + ... + a1n.xn <= b1
+// a21.x1 + a22.x2 + ... + a2n.xn <= b2
+//      ...
+// am1.x1 + am2.x2 + ... + amn.xn <= bm
+// x1, x2, ... , xn >= 0
+//
+// This can be rewritten in a matrix form, by setting:
+// x = (x1, x2, ... , xn)^T
+// c = (c1, c2, ... , cn)^T
+// b = (b1, b2, ... , bn)^T
+//     [ a11  a12  ...  a1n ]
+// A = | a21  a22  ...  a2n |
+//     |      ...           |
+//     [ am1  am2  ...  amn ]
+// With these definition we can rewrite the LP as
+// Maximize c.x
+// Subject to Ax <= b && x >= 0
+//
+// Minimizing c1.x1 + c2.x2 + ... + cn.xn is the same as maximizing -c1.x1 - c2.x2 - ... - cn.xn
+// You can raplace a11.x1 + a12.x2 + ... + a1n.xn >= b1 by -a11.x1 - a12.x2 - ... - a1n.xn <= -b1
+// An equality u = v is equivalent to the system of inequalities u <= v and u >= v.
+//
+// Returns -inf if there is no solution, inf if there are arbitrarily good solutions, or the maximum value of c.x otherwise.
+// The input vector is set to an optimal x (or in the unbounded case, an arbitrary solution fulfilling the constraints).
+// Numerical stability is not guaranteed. For better performance, define variables such that x = 0 is viable.
+// Time: O(NM * #pivots), where a pivot may be e.g. an edge relaxation. O(2^n) in the general case.
 #pragma once
 
 typedef double T; // long double, Rational, double + mod<P>...
