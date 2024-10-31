fc4f5fd484246aba9255a1123efbbeab162a6a31
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2024-10-31 18:06:48 +0600

    Update ConvexHull.h

--- content/geometry/ConvexHull.h
+++ content/geometry/ConvexHull.h
@@ -1,34 +1,17 @@
-/**
- * Author: Stjepan Glavina, chilli
- * Date: 2019-05-05
- * License: Unlicense
- * Source: https://github.com/stjepang/snippets/blob/master/convex_hull.cpp
- * Description:
-\\\begin{minipage}{75mm}
-Returns a vector of the points of the convex hull in counter-clockwise order.
-Points on the edge of the hull between two other points are not considered part of the hull.
-\end{minipage}
-\begin{minipage}{15mm}
-\vspace{-6mm}
-\includegraphics[width=\textwidth]{content/geometry/ConvexHull}
-\vspace{-6mm}
-\end{minipage}
- * Time: O(n \log n)
- * Status: stress-tested, tested with kattis:convexhull
-*/
-#pragma once
+typedef pair<int64_t, int64_t> pll;
 
-#include "Point.h"
-
-typedef Point<ll> P;
-vector<P> convexHull(vector<P> pts) {
-	if (sz(pts) <= 1) return pts;
-	sort(all(pts));
-	vector<P> h(sz(pts)+1);
-	int s = 0, t = 0;
-	for (int it = 2; it--; s = --t, reverse(all(pts)))
-		for (P p : pts) {
-			while (t >= s + 2 && h[t-2].cross(h[t-1], p) <= 0) t--;
+vector<pll> convexHull(vector<pll> pts) {
+	if (pts.size() <= 1) return pts;
+	sort(pts.begin(), pts.end());
+	vector<pll> h(pts.size()+1);
+	auto cross = [](pll p, pll a, pll b){
+		return (a.first-p.first)*(b.second-p.second) -
+			(a.second-p.second)*(b.first-p.first);
+	}; // Returns the cross product of vectors pa and pb
+	int s = 0, t = 0, i;
+	for (i = 2; i--; s = --t, reverse(pts.begin(), pts.end()))
+		for (pll& p : pts) {
+			while (t >= s + 2 && cross(h[t-2], h[t-1], p) <= 0) t--;
 			h[t++] = p;
 		}
 	return {h.begin(), h.begin() + t - (t == 2 && h[0] == h[1])};
