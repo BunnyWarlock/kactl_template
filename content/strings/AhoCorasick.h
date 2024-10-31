0f06d872ad90957256709c4b789d55e910769d35
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2024-10-31 17:45:50 +0600

    Update AhoCorasick.h

--- content/strings/AhoCorasick.h
+++ content/strings/AhoCorasick.h
@@ -1,68 +1,67 @@
-/**
- * Author: Simon Lindholm
- * Date: 2015-02-18
- * License: CC0
- * Source: marian's (TC) code
- * Description: Aho-Corasick automaton, used for multiple pattern matching.
- * Initialize with AhoCorasick ac(patterns); the automaton start node will be at index 0.
- * find(word) returns for each position the index of the longest word that ends there, or -1 if none.
- * findAll($-$, word) finds all words (up to $N \sqrt N$ many if no duplicate patterns)
- * that start at each position (shortest first).
- * Duplicate patterns are allowed; empty patterns are not.
- * To find the longest words that start at each position, reverse all input.
- * For large alphabets, split each symbol into chunks, with sentinel bits for symbol boundaries.
- * Time: construction takes $O(26N)$, where $N =$ sum of length of patterns.
- * find(x) is $O(N)$, where N = length of x. findAll is $O(NM)$.
- * Status: stress-tested
- */
-#pragma once
-
+template<char first = 'a', int alpha = 26>
 struct AhoCorasick {
-	enum {alpha = 26, first = 'A'}; // change this!
 	struct Node {
 		// (nmatches is optional)
-		int back, next[alpha], start = -1, end = -1, nmatches = 0;
+		int next[alpha], end = -1; // nmatches = 0;
+        // next contains the next node along with the failure links of each node
+        // end contains the index of the pattern which ends at that node or -1 otherwise
 		Node(int v) { memset(next, v, sizeof(next)); }
 	};
 	vector<Node> N;
-	vi backp;
-	void insert(string& s, int j) {
-		assert(!s.empty());
+	vector<int> backp, sizes;
+    // backp contains the dictionary links of each pattern
+ 
+	void insert(string& s, int j, vector<int>& start) {
+		// assert(!s.empty());
 		int n = 0;
 		for (char c : s) {
 			int& m = N[n].next[c - first];
-			if (m == -1) { n = m = sz(N); N.emplace_back(-1); }
+			if (m == -1){
+                n = m = N.size();
+                N.emplace_back(-1);
+                start.push_back(-1);
+            }
 			else n = m;
 		}
-		if (N[n].end == -1) N[n].start = j;
+		if (N[n].end == -1) start[n] = j;
 		backp.push_back(N[n].end);
 		N[n].end = j;
-		N[n].nmatches++;
+		// N[n].nmatches++;
 	}
-	AhoCorasick(vector<string>& pat) : N(1, -1) {
-		rep(i,0,sz(pat)) insert(pat[i], i);
-		N[0].back = sz(N);
+	AhoCorasick(vector<string>& pat): N(1, -1) {
+        vector<int> start(1, -1);
+		for(int i = 0; i < pat.size(); ++i){
+            insert(pat[i], i, start);
+            sizes.push_back(pat[i].size());
+        }
+        vector<int> back(N.size()+1);
+		back[0] = N.size();
 		N.emplace_back(0);
-
+        start.push_back(-1);
+ 
 		queue<int> q;
 		for (q.push(0); !q.empty(); q.pop()) {
-			int n = q.front(), prev = N[n].back;
-			rep(i,0,alpha) {
+			int n = q.front(), prev = back[n];
+			for (int i = 0; i < alpha; ++i) {
 				int &ed = N[n].next[i], y = N[prev].next[i];
 				if (ed == -1) ed = y;
 				else {
-					N[ed].back = y;
-					(N[ed].end == -1 ? N[ed].end : backp[N[ed].start])
+					back[ed] = y;
+					(N[ed].end == -1 ? N[ed].end : backp[start[ed]])
 						= N[y].end;
-					N[ed].nmatches += N[y].nmatches;
+					// N[ed].nmatches += N[y].nmatches;
 					q.push(ed);
 				}
 			}
 		}
 	}
-	vi find(string word) {
+ 
+    // find(word) returns for each position the index of the longest word that ends there, or -1 if none.
+    // find(x) is O(N), where N = length of x.
+	vector<int> find(string& word) {
 		int n = 0;
-		vi res; // ll count = 0;
+		vector<int> res; // ll count = 0;
+        // count is total no. of matches
 		for (char c : word) {
 			n = N[n].next[c - first];
 			res.push_back(N[n].end);
@@ -70,13 +69,20 @@ struct AhoCorasick {
 		}
 		return res;
 	}
-	vector<vi> findAll(vector<string>& pat, string word) {
-		vi r = find(word);
-		vector<vi> res(sz(word));
-		rep(i,0,sz(word)) {
+ 
+    // findAll(-, word) finds all words (up to N*sqrt(N) many if no duplicate patterns)
+    // that start at each position (shortest first).
+    // findAll is O(NM).
+	vector<vector<int>>findAll(string& word) {
+		vector<int> r = find(word);
+		vector<vector<int>> res(word.size());
+		// vector<int> res(sizes.size(), 0);
+        // ^ This version counts the freq of each pattern
+		for (int i = 0; i < word.size(); ++i) {
 			int ind = r[i];
 			while (ind != -1) {
-				res[i - sz(pat[ind]) + 1].push_back(ind);
+                res[i - sizes[ind] + 1].push_back(ind);
+				// ++res[ind];
 				ind = backp[ind];
 			}
 		}
