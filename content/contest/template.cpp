bb4d3180919ddd0f29264b076d113cda8477f6f0
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2025-05-09 17:45:52 +0600

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
@@ -8,7 +7,12 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
+template <class T1, class T2> ostream& operator<<(ostream& out, const pair<T1, T2> p)
+{ return out<<'('<<p.first<<", "<<p.second<<")"; }
+template <class ...Args> auto &print(const Args &...args)
+{ return ((cerr<<args<<", "), ...)<<"\b\b)"<<endl; }
+#define watch(...) if(true){cerr<<"("#__VA_ARGS__") = ("; print(__VA_ARGS__);}
+
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
-}
+	cin.exceptions(cin.failbit); }
