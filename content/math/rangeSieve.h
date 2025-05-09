0b60cd91ed76abb0e9d860038cc67e1832db5cd6
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2025-05-09 17:42:50 +0600

    Update rangeSieve.h

--- content/math/rangeSieve.h
+++ content/math/rangeSieve.h
@@ -20,9 +20,4 @@ void sieveRange(ll l, ll r){
     for (ll j = max(i*1LL*i, (l+i-1)/ i*1LL*i); j <= r; j += i)
       rangePrime[j-l]= false;
   if (l == 1) rangePrime[0] = false;
-
-  // Problem Specific. Can Ignore.
-  for (int i = 0; i < r-l+1; ++i)
-    if (rangePrime[i]) cout<<i+l<<endl;
-  cout<<endl;
 }
